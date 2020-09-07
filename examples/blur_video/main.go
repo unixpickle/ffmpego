8c0fcd25186248c81af0d59b019994c2f3425194
Alex Nichol <unixpickle@gmail.com>
2020-09-07 16:08:01 -0400

    speedup blur by factoring gaussian kernel
    Reduces the time complexity of the filter from O(R^2) to O(R)

--- examples/blur_video/main.go
+++ examples/blur_video/main.go
@@ -72,19 +72,19 @@ type GaussianKernel struct {
 func NewGaussianKernel(radius int, sigma float32) *GaussianKernel {
 	res := &GaussianKernel{
 		Radius: radius,
-		Data:   make([]float32, 0, (radius*2+1)*(radius*2+1)),
+		Data:   make([]float32, 0, radius*2+1),
 	}
-	for y := -radius; y <= radius; y++ {
-		for x := -radius; x <= radius; x++ {
-			intensity := math.Exp(-float64(x*x+y*y) / float64(sigma*sigma))
-			res.Data = append(res.Data, float32(intensity))
-		}
+	for x := -radius; x <= radius; x++ {
+		intensity := math.Exp(-float64(x*x) / float64(sigma*sigma))
+		res.Data = append(res.Data, float32(intensity))
 	}
 	return res
 }
 
 func (g *GaussianKernel) Filter(img image.Image) image.Image {
 	b := img.Bounds()
+	width := b.Dx()
+	height := b.Dy()
 
 	var colors [][3]float32
 	for y := b.Min.Y; y < b.Max.Y; y++ {
@@ -98,47 +98,71 @@ func (g *GaussianKernel) Filter(img image.Image) image.Image {
 		}
 	}
 
-	result := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
-	var wg sync.WaitGroup
-	numGos := runtime.GOMAXPROCS(0)
-	for i := 0; i < numGos; i++ {
-		wg.Add(1)
-		go func(goIdx int) {
-			defer wg.Done()
+	mapY := func(f func(y int)) {
+		var wg sync.WaitGroup
+		numGos := runtime.GOMAXPROCS(0)
+		for i := 0; i < numGos; i++ {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				for y := i; y < height; y += numGos {
+					f(y)
+				}
+			}(i)
+		}
+		wg.Wait()
+	}
 
-			for y := goIdx; y < b.Dy(); y += numGos {
-				for x := 0; x < b.Dx(); x++ {
-					var kernelIdx int
-					var kernelSum float32
-					var colorSum [3]float32
-					for dy := -g.Radius; dy <= g.Radius; dy++ {
-						sumY := y + dy
-						for dx := -g.Radius; dx <= g.Radius; dx++ {
-							sumX := x + dx
-							if sumY >= 0 && sumY < b.Dy() && sumX >= 0 && sumX < b.Dx() {
-								k := g.Data[kernelIdx]
-								c := colors[sumX+b.Dx()*sumY]
-								for i, x := range c {
-									colorSum[i] += x * k
-								}
-								kernelSum += k
-							}
-							kernelIdx++
-						}
+	sums := make([]float32, len(colors))
+	intermediate := make([][3]float32, len(colors))
+	mapY(func(y int) {
+		for x := 0; x < width; x++ {
+			var kernelSum float32
+			var colorSum [3]float32
+			for dy := -g.Radius; dy <= g.Radius; dy++ {
+				sumY := y + dy
+				if sumY >= 0 && sumY < height {
+					k := g.Data[dy+g.Radius]
+					c := colors[x+width*sumY]
+					for i, x := range c {
+						colorSum[i] += x * k
 					}
-					for i := range colorSum {
-						colorSum[i] /= kernelSum
-					}
-					result.SetRGBA(x, y, color.RGBA{
-						R: uint8(colorSum[0] * 255.999),
-						G: uint8(colorSum[1] * 255.999),
-						B: uint8(colorSum[2] * 255.999),
-						A: 0xff,
-					})
+					kernelSum += k
 				}
 			}
-		}(i)
-	}
-	wg.Wait()
+			idx := x + y*b.Dx()
+			intermediate[idx] = colorSum
+			sums[idx] = kernelSum
+		}
+	})
+
+	result := image.NewRGBA(image.Rect(0, 0, width, height))
+	mapY(func(y int) {
+		for x := 0; x < b.Dx(); x++ {
+			var kernelSum float32
+			var colorSum [3]float32
+			for dx := -g.Radius; dx <= g.Radius; dx++ {
+				sumX := x + dx
+				if sumX >= 0 && sumX < width {
+					k := g.Data[dx+g.Radius]
+					c := intermediate[sumX+width*y]
+					for i, x := range c {
+						colorSum[i] += x * k
+					}
+					kernelSum += k * sums[x+width*y]
+				}
+			}
+			for i := range colorSum {
+				colorSum[i] /= kernelSum
+			}
+			result.SetRGBA(x, y, color.RGBA{
+				R: uint8(colorSum[0] * 255.999),
+				G: uint8(colorSum[1] * 255.999),
+				B: uint8(colorSum[2] * 255.999),
+				A: 0xff,
+			})
+		}
+	})
+
 	return result
 }
